87042fa900a3c24620242e18d7caf7fc25592fb6
Lewis Crichton <lewistehminerz@outlook.com>
2021-03-25 17:38:01 +0000

    fixes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,14 +2,7 @@
   import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
 </script>
 
-<style>
-	#top-bar {
-		background-color: #fff;
-		color: #000;
-	}
-</style>
-
-<TopAppBar variant="fixed" id="top-bar">
+<TopAppBar variant="fixed" color="secondary">
 	<Row>
 		<Section>
 			<Title href="/">
